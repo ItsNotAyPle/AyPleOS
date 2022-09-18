e3e8c953eb3e89eb6e534267681de8e9325c7b03
Adam <rawritsadam90@gmail.com>
2022-09-18 13:53:10 +0100

    fixed my copying of code

--- src/includes/io.h
+++ src/includes/io.h
@@ -1,25 +1,26 @@
 #ifndef KERNEL_IO_H
 #define KERNEL_IO_H
 
+#include "types.h"
 
 // https://github.com/lucianoforks/tetris-os/blob/master/src/util.h
-static inline u16 inports(u16 port) {
-    u16 r;
+static inline uint16 inports(uint16 port) {
+    uint16 r;
     asm("inw %1, %0" : "=a" (r) : "dN" (port));
     return r;
 }
 
-static inline void outports(u16 port, u16 data) {
+static inline void outports(uint16 port, uint16 data) {
     asm("outw %1, %0" : : "dN" (port), "a" (data));
 }
 
-static inline u8 inportb(u16 port) {
-    u8 r;
+static inline uint8 inportb(uint16 port) {
+    uint8 r;
     asm("inb %1, %0" : "=a" (r) : "dN" (port));
     return r;
 }
 
-static inline void outportb(u16 port, u8 data) {
+static inline void outportb(uint16 port, uint8 data) {
     asm("outb %1, %0" : : "dN" (port), "a" (data));
 }
 
